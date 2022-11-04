88597cbbbf57224658af4bab5d2b6b16d1b9faec
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2022-11-04 17:26:37 +0100

    added faker

--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Seeder;
 
 use Faker\Generator as Faker;
 
+use App\Train;
+
 class TrainsTableSeeder extends Seeder
 {
     /**
@@ -11,8 +13,25 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         //
+        $tempCode = 'ITxaxa';
+        for($i = 0; $i < 100; $i++){
+
+            $train = new Train();
+            $train->azienda = $faker->name();
+            $train->stazione_partenza = $faker->city();
+            $train->stazione_arrivo = $faker->city();
+            $train->orario_partenza = $faker->time();
+            $train->orario_arrivo = $faker->time();
+            $randomDate = $faker->date();
+            $train->data_partenza = $randomDate;
+            $train->data_arrivo = $randomDate;
+            $train->codice_treno = $tempCode . $i;
+            $train->save();
+        }
+
+
     }
 }
